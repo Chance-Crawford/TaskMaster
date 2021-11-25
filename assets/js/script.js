c5e89ddccc55f87b365c7017d5185c98b6921b73
Chance <chancecrawford30@gmail.com>
2021-11-24 21:21:28 -0800

    added ability to move tasks and save tasks location in lists after each update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+
+// an object which will be used to save all tasks
+// to localStorage for recovery.
+// this tasks object will have 4 arrays. The arrays are to do, in progress, etc.
+// those arrays will house all TASK OBJECTS based on their status.
 var tasks = {};
 
 var createTask = function(taskText, taskDate, taskList) {
@@ -235,6 +240,159 @@ $(".list-group").on("blur", "input[type='text']", function() {
 });
 
 
+// jquery ui sortable allows for you to drag elements
+// and resort them in other lists, or in the same list.
+// in this case we are selecting all of the ui elements like
+// the to do list, inprogress list, etc. and are making them sortable.
+// sortable in jquery means that all child elements (li) within the
+// sortable list can be can be dragged and resorted within the list
+$(".card .list-group").sortable({
+  // we are allowing the list to connect with
+  // any other ul list. This means any items in one list can be sorted into another
+  // sortable ul list-group list, since they are connected.
+  connectWith: $(".card .list-group"),
+  // we are now adding several event listeners based on the state
+  // of sorting or dragging.
+  scroll: false,
+  tolerance: "pointer",
+  // tells jQuery to create a copy of the dragged element and move the copy 
+  // instead of the original. This is necessary to prevent click events from 
+  // accidentally triggering on the original element.
+  helper: "clone",
+  // The activate and deactivate events trigger once for all connected 
+  // lists as soon as dragging starts and stops.
+  // Events like activate and over would be great for styling. We could change 
+  // the color of elements at each step to let the user know dragging is 
+  // working correctly.
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  // The over and out events trigger when a dragged item enters or 
+  // leaves a connected list.
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  // The update event triggers when the contents of a list have 
+  // changed (e.g., the items were re-ordered, an item was removed, or an 
+  // item was added).
+  // an updated list signifies the need to re-save tasks in localStorage. 
+  // Remember that an update can happen to two lists at once if a task is 
+  // dragged from one column to another.
+  // In this case when you move a task from one list to another,
+  // update will run for both columns individually.
+  // for removing the task from one list and placing it in the other,
+  // the update will run for each list. Updating and saving the new 
+  // list to the localStorage.
+  update: function(event) {
+    // array to store the task data in after an update.
+    // temp array will overwrite whats currently saved in the tasks object.
+    var tempArr = [];
+    // after update has occured,
+    // loop over current set of children (li) in sortable list
+    // loop over each child in the list and perform this function.
+    // The children() method returns an array of the ul element's, li children
+    $(this).children().each(function() {
+      // here "this" refers to the child task li element at the current index.
+      // find the p element within the current li task, and get the inner text content
+      // from the p element.
+      // trim any of the whitespace before or after it just in case, 
+      // and store it in the variable
+      // "text".
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+
+    });
+
+    // The next step is to use the tempArr array to overwrite what's currently 
+    // saved in the tasks object. We're dealing with multiple lists, though, not 
+    // just toDo. How do we know when to update tasks.toDo versus tasks.inReview 
+    // or tasks.done?
+
+    // We solved a similar problem earlier in the project when dealing with 
+    // blur events. Each list has an id attribute that matches a property on tasks. 
+    // For instance: id="list-inReview". We'll use the same approach here to get 
+    // the list's ID and update the corresponding array on tasks
+    // trim down list's ID to match the name of the array we want 
+    // within the tasks object.
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array in tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+
+
+  }
+
+});
+
+
+// deletes the task when the task is dropped over the red
+// delete zone.
+// droppable is jquery ui method
+// select delete zone, make it droppable.
+$("#trash").droppable({
+  // Controls which draggable elements are accepted by the droppable.
+  // the delete zone will only accept the task elements within the unordered
+  // lists. (the tasks are li elements).
+  // we made the li elements draggable in the function above.
+  // technically we made the ul the tasks are held in sortable, which makes the
+  // tasks themselves draggable.
+  accept: ".card .list-group-item",
+  // Specifies which mode to use for testing whether 
+  // a draggable is hovering over a droppable.
+  // "touch": Draggable overlaps the droppable any amount.
+  // to see more modes look at jquery ui documentation - droppable.
+  tolerance: "touch",
+  // Triggered when an accepted draggable is dropped on the droppable 
+  // (based on the tolerance option).
+  // the drop method, in this case, is most important because that 
+  // means a user is trying to delete a task.
+  drop: function(event, ui) {
+    // jQuery UI has a second function parameter, which they've decided to 
+    // call ui. This variable is an object that contains a property called 
+    // draggable. According to the documentation, draggable is "a jQuery 
+    // object representing the draggable element." If that's true, then 
+    // we should be able to call DOM methods on it!
+    // jQuery's remove() method works just like a regular JavaScript 
+    // remove() and will remove that element from the DOM entirely.
+
+    // Now that the task has been removed, do we need to call saveTasks() 
+    // again? Surprisingly, no! Removing a task from any of the lists 
+    // triggers a sortable update(), it triggers the sortable function 
+    // that we defined above, and considers the removal as an "update" 
+    // within the function meaning the sortable calls 
+    // saveTasks() for us.
+    ui.draggable.remove();
+  },
+  // defined in above function.
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
 
 
 
